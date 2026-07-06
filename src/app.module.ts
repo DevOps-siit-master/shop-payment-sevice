be7ad53e8eea337a7ff85bf560ed666f99e23bc5
Miloš Bojanić <104066997+milosbojanic@users.noreply.github.com>
2026-07-06 20:05:28 +0200

    feat: verify USDT payments on-chain and mark orders paid (#2)
    * feat: verify USDT payments on-chain and mark orders paid
    
    * test: add unit tests dor payment verification

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PaymentsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
