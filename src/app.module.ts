3bd2d448876dd5826db6d89e9759298602751440
Milos <mbojanic53@gmail.com>
2026-07-07 17:35:16 +0200

    feat: expose prometheus metrics for http traffic

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,24 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PaymentsModule } from './payments/payments.module';
+import { MetricsController } from './metrics/metrics.controller';
+import { MetricsMiddleware } from './metrics/metrics.middleware';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PaymentsModule
   ],
-  controllers: [AppController],
+  controllers: [AppController, MetricsController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(MetricsMiddleware)
+      .exclude({ path: 'metrics', method: RequestMethod.ALL })
+      .forRoutes('*');
+  }
+}
