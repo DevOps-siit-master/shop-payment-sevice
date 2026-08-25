82dce52b4c20a26297a83ca56e538c46d00f2a67
Milos <mbojanic53@gmail.com>
2026-08-25 19:57:00 +0200

    refactor(payments): inject the RPC provider and config through DI

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
+import { ethProviderFactory } from './eth-provider';
 
 @Module({
   controllers: [PaymentsController],
-  providers: [PaymentsService],
+  providers: [PaymentsService, ethProviderFactory],
 })
 export class PaymentsModule {}
